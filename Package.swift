7f5c4549dc1df4bf076734ff01bf89826e5d34cb
Tanner Nelson <tanner@bluebite.com>
2016-01-27 17:43:54 -0500

    add package

---
+++ Package.swift
@@ -0,0 +1,8 @@
+import PackageDescription
+
+let package = Package(
+    name: "Fluent",
+    dependencies: [ 
+        //.Package(url: "https://github.com/kylef/Spectre.git", majorVersion: 0) //testing
+    ]
+)
