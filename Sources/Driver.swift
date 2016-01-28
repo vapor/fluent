99b624f4132083dbb165c731a9054172c9e76269
Art Kay <art@artkay.nyc>
2016-01-28 00:21:36 -0500

    Driver protocol

--- Sources/Driver.swift
+++ Sources/Driver.swift
@@ -4,4 +4,8 @@ protocol Driver {
 	func delete(table table: String, filters: [Filter])
 	func update(table table: String, filters: [Filter], data: [String: String])
 	func insert(table table: String, items: [[String: String]])
+	func upsert(table table: String, items: [[String: String]])
+	func exists(table table: String, filters: [Filter]) -> Bool
+	func count(table table: String, filters: [Filter]) -> Int
+
 }
