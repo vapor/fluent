ef3ceaead48735626014a734e1d89e80f95298eb
Tanner Nelson <tanner@bluebite.com>
2016-02-21 17:24:29 -0500

    naming

--- Package.swift
+++ Package.swift
@@ -3,7 +3,5 @@ import PackageDescription
 let package = Package(
     name: "Fluent",
     dependencies: [ 
-        //.Package(url: "https://github.com/kylef/Spectre.git", majorVersion: 0) //testing
-        //.Package(url: "https://github.com/tannernelson/csqlite.git", majorVersion: 0)
     ]
 )
