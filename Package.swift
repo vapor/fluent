7b01423e029e664631898ef9c78705b382053838
d_abe <abe@flup.jp>
2016-02-29 10:47:21 +0900

    change LoggerAPI version

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Fluent",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", versions: Version(0,2,0)..<Version(0,3,0)),
+        .Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", versions: Version(0,3,0)..<Version(0,4,0)),
     ]
 )
