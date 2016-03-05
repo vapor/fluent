c4f0cda98b7ad21f356498a2fc01eae4735afc02
Daijiro Abe <abe@flup.jp>
2016-03-05 16:26:30 +0900

    delete dependency

--- Package.swift
+++ Package.swift
@@ -3,6 +3,5 @@ import PackageDescription
 let package = Package(
     name: "Fluent",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", versions: Version(0,3,0)..<Version(0,4,0)),
     ]
 )
