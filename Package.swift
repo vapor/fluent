a54046a9ce5258007d1c59a0df38a996b2edffbf
Tanner Nelson <tanner@bluebite.com>
2016-05-18 15:03:52 -0400

    update package

--- Package.swift
+++ Package.swift
@@ -10,23 +10,9 @@ let package = Package(
             ]
         ),
         Target(
-            name: "FluentSQLite",
-            dependencies: [
-                .Target(name: "Fluent")
-            ]
-        ),
-        Target(
-            name: "FluentMongo",
-            dependencies: [
-                .Target(name: "Fluent")
-            ]
-        ),
-        Target(
             name: "FluentDev",
             dependencies: [
                 .Target(name: "Fluent"),
-                .Target(name: "FluentSQLite"),
-                .Target(name: "FluentMongo"),
             ]
         ),
     ]
