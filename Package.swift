c3352a6f191d135b56f6baecc46b5543e3cfa0c0
Tanner Nelson <me@tanner.xyz>
2016-07-25 11:47:58 -0400

    idk

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Fluent",
     dependencies: [
-		// Data structure for converting between multiple representations
-		.Package(url: "https://github.com/qutheory/node.git", majorVersion: 0, minor: 2)
+        // Data structure for converting between multiple representations
+        .Package(url: "https://github.com/qutheory/node.git", majorVersion: 0, minor: 2)
     ]
 )
