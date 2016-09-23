89d1e2b39956db65af7e67d7548b0000bbba22cb
Tanner Nelson <me@tanner.xyz>
2016-09-22 20:20:30 -0400

    fix print

--- Sources/Fluent/Preparation/Migration.swift
+++ Sources/Fluent/Preparation/Migration.swift
@@ -12,7 +12,6 @@ final class Migration: Entity {
     init(node: Node, in context: Context) throws {
         id = try node.extract("id")
         name = try node.extract("name")
-        print(exists)
     }
 
     func makeNode(context: Context = EmptyNode) throws -> Node {
