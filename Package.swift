d73095fd362eb7d5806770f0094cfd74f2498518
Logan Wright <logan.william.wright@gmail.com>
2017-03-27 17:23:22 +0100

    deps update

--- Package.swift
+++ Package.swift
@@ -1,5 +1,6 @@
 import PackageDescription
 
+let beta = Version(2,0,0, prereleaseIdentifiers: ["alpha"])
 let package = Package(
     name: "Fluent",
     targets: [
@@ -8,12 +9,12 @@ let package = Package(
     ],
     dependencies: [
         // Data structure for converting between multiple representations
-        .Package(url: "https://github.com/vapor/node.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"])),
+        .Package(url: "https://github.com/vapor/node.git", beta),
 
         // Core Components
-        .Package(url: "https://github.com/vapor/core.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"])),
+        .Package(url: "https://github.com/vapor/core.git", beta),
 
         // In memory Database
-        .Package(url: "https://github.com/vapor/sqlite.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"])),
+        .Package(url: "https://github.com/vapor/sqlite.git", beta),
     ]
 )
