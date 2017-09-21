ea2f22d3faa4853b9b178d2acaabc56effb0c1da
Egor Zhdan <egor.zhdan@gmail.com>
2017-09-21 21:56:10 +0300

    Move Box class to utilities

--- Tests/FluentTests/ModelTests.swift
+++ Tests/FluentTests/ModelTests.swift
@@ -97,43 +97,6 @@ class ModelTests: XCTestCase {
         do { try test.save() } catch {}
         XCTAssert(test.id != nil)
     }
-
-    final class Box : Entity, Preparation {
-        
-        var name: String
-        var weight: Int
-        let storage = Storage()
-        
-        init(name: String, weight: Int) {
-            self.name = name
-            self.weight = weight
-        }
-        
-        init(row: Row) throws {
-            name = try row.get("name")
-            weight = try row.get("weight")
-        }
-        
-        func makeRow() throws -> Row {
-            var row = Row()
-            try row.set("name", name)
-            try row.set("weight", weight)
-            return row
-        }
-        
-        static func prepare(_ database: Database) throws {
-            try database.create(self) {
-                builder in
-                builder.id()
-                builder.string("name")
-                builder.int("weight")
-            }
-        }
-        
-        static func revert(_ database: Database) throws {
-            try database.delete(self)
-        }
-    }
     
     func testSaveOnlyUpdatesDirty() {
         Box.database = db
