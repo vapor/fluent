6efe49ef8c362c105d950a24b94a0ce96600b8c8
Tanner Nelson <me@tanner.xyz>
2017-12-19 16:37:11 -0500

    export fluent benchmark

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ let package = Package(
     name: "Fluent",
     products: [
         .library(name: "Fluent", targets: ["Fluent"]),
+        .library(name: "FluentBenchmark", targets: ["FluentBenchmark"]),
         .library(name: "FluentSQL", targets: ["FluentSQL"]),
         .library(name: "FluentSQLite", targets: ["FluentSQLite"]),
         .library(name: "SQL", targets: ["SQL"]),
