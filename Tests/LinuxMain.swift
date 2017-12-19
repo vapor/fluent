948649b612bf74ed47bc8fd1fbeabd0f16c0f19f
Tanner Nelson <me@tanner.xyz>
2017-12-19 11:08:27 -0500

    fix ci

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -2,10 +2,19 @@
 
 import XCTest
 @testable import FluentTests
+@testable import SQLiteTests
+@testable import SQLTests
 
 XCTMain([
+    // Fluent
     testCase(SQLiteBenchmarkTests.allTests),
-    testCase(FluentMySQLTests.allTests),
+
+    // SQlite
+    testCase(SQLiteTests.allTests),
+
+    // SQL
+    testCase(DataTests.allTests),
+    testCase(SchemaTests.allTests),
 ])
 
 #endif
