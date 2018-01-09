0e21ce7aa2b01dcb2b02babf6fdac9db8f71dae8
Tanner Nelson <me@tanner.xyz>
2018-01-09 18:49:40 -0500

    provider rename

--- Sources/FluentSQLite/SQLiteProvider.swift
+++ Sources/FluentSQLite/SQLiteProvider.swift
@@ -1,8 +1,11 @@
 import Service
 import SQLite
 
+@available(*, unavailable, renamed: "FluentSQLiteProvider")
+public typealias SQLiteProvider = FluentSQLiteProvider
+
 /// Registers and boots SQLite services.
-public final class SQLiteProvider: Provider {
+public final class FluentSQLiteProvider: Provider {
     /// See Provider.repositoryName
     public static let repositoryName = "fluent-sqlite"
 
@@ -15,7 +18,7 @@ public final class SQLiteProvider: Provider {
             return SQLiteConfig()
         }
         services.register(SQLiteDatabase.self) { container -> SQLiteDatabase in
-            let storage = try container.make(SQLiteStorage.self, for: SQLiteProvider.self)
+            let storage = try container.make(SQLiteStorage.self, for: FluentSQLiteProvider.self)
             return SQLiteDatabase(storage: storage)
         }
     }
