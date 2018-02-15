5f6157911fcb4e43075c69d637a3a5dfd62c476a
Tanner Nelson <me@tanner.xyz>
2018-02-14 21:53:09 -0500

    prerelease version updates

--- Package.swift
+++ Package.swift
@@ -12,19 +12,19 @@ let package = Package(
     ],
     dependencies: [
         // Swift Promises, Futures, and Streams.
-        .package(url: "https://github.com/vapor/async.git", .exact("1.0.0-beta.1")),
+        .package(url: "https://github.com/vapor/async.git", "1.0.0-beta.1"..<"1.0.0-beta.2"),
 
         // Core extensions, type-aliases, and functions that facilitate common tasks.
-        .package(url: "https://github.com/vapor/core.git", .exact("3.0.0-beta.1")),
+        .package(url: "https://github.com/vapor/core.git", "3.0.0-beta.1"..<"3.0.0-beta.2"),
 
         // Swift wrapper for Console I/O
-        .package(url: "https://github.com/vapor/console.git", .exact("3.0.0-beta.1")),
+        .package(url: "https://github.com/vapor/console.git", "3.0.0-beta.1"..<"3.0.0-beta.2"),
 
         // Core services for creating database integrations.
-        .package(url: "https://github.com/vapor/database-kit.git", .exact("1.0.0-beta.2")),
+        .package(url: "https://github.com/vapor/database-kit.git", "1.0.0-beta.2"..<"1.0.0-beta.3"),
 
         // Service container and configuration system.
-        .package(url: "https://github.com/vapor/service.git", .exact("1.0.0-beta.1")),
+        .package(url: "https://github.com/vapor/service.git", "1.0.0-beta.1"..<"1.0.0-beta.2"),
     ],
     targets: [
         .target(name: "CSQLite"),
