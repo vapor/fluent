921c109bd8d26ab51ffe57f0fd3330bbb547f16d
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2018-03-14 17:01:59 +0000

    PR comments done

--- Sources/Fluent/Query/QueryGroupBy.swift
+++ Sources/Fluent/Query/QueryGroupBy.swift
@@ -1,25 +1,31 @@
 import CodableKit
 
 /// GROUP BY statement for QueryBuilder
-public enum QueryGroupBy {
-    case field(QueryField)
+public struct QueryGroupBy {
+    public enum Storage {
+        case field(QueryField)
+    }
+    
+    /// Internal storage
+    public let storage: Storage
+    
+    /// Generates QueryGroupBy object for a field
+    public static func field(_ field: QueryField) -> QueryGroupBy { return .init(storage: .field(field)) }
+    
 }
 
 // MARK: Builder
 
 extension QueryBuilder {
     /// Add a Group By to the Query.
-    public func groupBy<T>(_ field: KeyPath<Model, T>) -> Self
-        where T: KeyStringDecodable
-    {
-        let groupBy = QueryGroupBy.field(field.makeQueryField())
-        return self.groupBy(groupBy)
+    public func group<T>(by field: KeyPath<Model, T>) throws -> Self where T: KeyStringDecodable {
+        let groupBy = try QueryGroupBy.field(field.makeQueryField())
+        return self.group(by: groupBy)
     }
     
     /// Add a Group By to the Query.
-    public func groupBy(_ groupBy: QueryGroupBy) -> Self {
+    public func group(by groupBy: QueryGroupBy) -> Self {
         query.groups.append(groupBy)
         return self
     }
 }
-
--- Sources/FluentSQL/QueryGroupBy.swift
+++ Sources/FluentSQL/QueryGroupBy.swift
@@ -4,7 +4,7 @@ import SQL
 extension QueryGroupBy {
     /// Convert query group by to sql group by.
     internal func makeDataGroupBy() -> DataGroupBy {
-        switch self {
+        switch storage {
         case .field(let field):
             return DataGroupBy.column(field.makeDataColumn())
         }
