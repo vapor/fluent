d874d05dd12a02c56c2e5310a15d48603f5fb0f0
tanner0101 <me@tanner.xyz>
2018-03-22 15:06:18 -0400

    core rc 2.1 updates

--- Sources/Fluent/Model/ID.swift
+++ Sources/Fluent/Model/ID.swift
@@ -8,28 +8,3 @@ public protocol ID: Codable, Equatable, KeyStringDecodable { }
 extension Int: ID { }
 extension String: ID { }
 extension UUID: ID { }
-
-
-/// MARK: String
-
-extension Int: StringDecodable {
-    /// See StringDecodable.decode
-    public static func decode(from string: String) -> Int? {
-        return Int(string)
-    }
-}
-
-extension UUID: StringDecodable {
-    /// See StringDecodable.decode
-    public static func decode(from string: String) -> UUID? {
-        return UUID(uuidString: string)
-    }
-}
-
-extension String: StringDecodable {
-    /// See StringDecodable.decode
-    public static func decode(from string: String) -> String? {
-        return string
-    }
-}
-
