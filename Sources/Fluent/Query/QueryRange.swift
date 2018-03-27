c445a76c7d74b4df89c6f323c11de108a6471734
tanner0101 <me@tanner.xyz>
2018-03-27 17:31:56 -0400

    add closed range, fixes #427

--- Sources/Fluent/Query/QueryRange.swift
+++ Sources/Fluent/Query/QueryRange.swift
@@ -1,17 +1,17 @@
 /// Defines a Fluent query limit and offset.
 public struct QueryRange {
-    /// The lower bound of item indexes to return.
-    /// This should be 0 by default.
+    /// The lower bound of item indexes to return. This should be `0` by default.
     public var lower: Int
 
-    /// The upper bound of item indexes to return.
-    /// If this is nil, the range acts as just on offset.
-    /// If it is set, the number of results will have a max
-    /// possible value.
+    /// The upper bound of item indexes to return. If this is `nil`, the range acts as just on offset.
+    /// If it is set, the number of results will have a max possible value (upper - lower).
     public var upper: Int?
 
     /// Creates a new limit with a count and offset.
-    /// See Limit.count and Limit.offset.
+    ///
+    /// - parameters:
+    ///     - lower: Amount to offset the query by.
+    ///     - upper: `upper` - `lower` = maximum results.
     public init(lower: Int, upper: Int?) {
         self.lower = lower
         self.upper = upper
@@ -19,27 +19,51 @@ public struct QueryRange {
 }
 
 extension QueryBuilder {
-    /// Limits the query to a range of indexes.
+    /// Limits the results of this query to the specified range.
+    ///
+    ///     query.range(2..<5) // returns at most 3 results, offset by 2
+    ///
     public func range(_ range: Range<Int>) -> Self {
         return self.range(lower: range.lowerBound, upper: range.upperBound)
     }
 
-    /// Limits the query to a max number of results.
+    /// Limits the results of this query to the specified range.
+    ///
+    ///     query.range(...5) // returns at most 6 results
+    ///
     public func range(_ range: PartialRangeThrough<Int>) -> Self {
         return self.range(upper: range.upperBound)
     }
 
-    /// Limits the query to a max number of results.
+    /// Limits the results of this query to the specified range.
+    ///
+    ///     query.range(..<5) // returns at most 5 results
+    ///
     public func range(_ range: PartialRangeUpTo<Int>) -> Self {
         return self.range(upper: range.upperBound - 1)
     }
 
-    /// Offsets the query by the supplied index.
+    /// Limits the results of this query to the specified range.
+    ///
+    ///     query.range(5...) // offsets the result by 5
+    ///
     public func range(_ range: PartialRangeFrom<Int>) -> Self {
         return self.range(lower: range.lowerBound)
     }
 
-    /// Convenience for applying a range to the query.
+    /// Limits the results of this query to the specified range.
+    ///
+    ///     query.range(2..<5) // returns at most 3 results, offset by 2
+    ///
+    public func range(_ range: ClosedRange<Int>) -> Self {
+        return self.range(lower: range.lowerBound, upper: range.upperBound)
+    }
+
+    /// Limits the results of this query to the specified range.
+    ///
+    /// - parameters:
+    ///     - lower: Amount to offset the query by.
+    ///     - upper: `upper` - `lower` = maximum results.
     public func range(lower: Int = 0, upper: Int? = nil) -> Self {
         let limit = QueryRange(lower: lower, upper: upper)
         query.range = limit
