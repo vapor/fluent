a29140efab055842f1d039ccf7851a640f2d381b
Alex Reilly <alexander.r.reilly@gmail.com>
2018-05-02 22:55:43 -0700

    Added comments

--- Sources/Fluent/Model/Collection+Model.swift
+++ Sources/Fluent/Model/Collection+Model.swift
@@ -1,6 +1,10 @@
 import Async
 
 extension Collection where Element: Model, Element.Database: QuerySupporting {
+    /// Saves a collection of models to Database
+    ///
+    /// - Parameter conn: A means to connect to a database
+    /// - Returns: A future array of the saved models
     public func save(on conn: DatabaseConnectable) -> Future<[Element]> {
         return self.map { $0.save(on: conn) }.flatten(on: conn)
     }
