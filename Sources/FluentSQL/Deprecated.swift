0f65e3adeaadd5a8ccd3025219bd317481c8e8d9
tanner0101 <me@tanner.xyz>
2018-05-30 16:14:35 -0400

    deprecations + benchmark names

--- Sources/FluentSQL/Deprecated.swift
+++ Sources/FluentSQL/Deprecated.swift
@@ -24,5 +24,18 @@ extension SchemaBuilder {
     }
 }
 
+extension SchemaBuilder {
+    /// - warning: Deprecated.
+    @available(*, deprecated, renamed: "unique(on:)")
+    public func addIndex<T>(to: KeyPath<Model, T>, isUnique: Bool = false) {
+        return unique(on: to)
+    }
+    /// - warning: Deprecated.
+    @available(*, deprecated, renamed: "unique(on:_:)")
+    public func addIndex<A, B>(to: KeyPath<Model, A>, _ and: KeyPath<Model, B>, isUnique: Bool = false) {
+        return unique(on: to, and)
+    }
+}
+
 @available(*, deprecated, renamed: "SQLDatabase")
 public typealias SchemaSupporting = SQLSupporting
