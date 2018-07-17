6f67347dabc833905e52a2fa83344f09afeb8606
tanner0101 <me@tanner.xyz>
2018-07-16 21:12:54 -0400

    add missing doc blocks

--- Sources/FluentSQL/SchemaBuilder+Field.swift
+++ Sources/FluentSQL/SchemaBuilder+Field.swift
@@ -2,6 +2,14 @@ extension SchemaBuilder where
     Model.Database.Schema: FluentSQLSchema,
     Model.Database.SchemaField == Model.Database.Schema.ColumnDefinition
 {
+    /// Adds a field with specified type and constraints.
+    ///
+    ///     builder.field(for: \.name, type: ..., ...)
+    ///
+    /// - parameters:
+    ///     - key: `KeyPath` to the field.
+    ///     - type: Data type to use for this field.
+    ///     - constraints: Constraints to apply to this field.
     public func field<T>(
         for key: KeyPath<Model, T>,
         type: Model.Database.Schema.ColumnDefinition.DataType,
@@ -11,6 +19,14 @@ extension SchemaBuilder where
         self.field(.columnDefinition(.column(nil, .identifier(property.path[0])), type, [.notNull] + constraints))
     }
     
+    /// Adds a field with specified type and constraints.
+    ///
+    ///     builder.field(for: \.name, type: ..., ...)
+    ///
+    /// - parameters:
+    ///     - key: `KeyPath` to the field.
+    ///     - type: Data type to use for this field.
+    ///     - constraints: Constraints to apply to this field.
     public func field<T>(
         for key: KeyPath<Model, T?>,
         type: Model.Database.Schema.ColumnDefinition.DataType,
