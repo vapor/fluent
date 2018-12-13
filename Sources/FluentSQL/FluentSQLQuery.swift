33b7d30ec326ef794fe3547c848d7357c70aafef
Toby Griffin <toby.griffin@gmail.com>
2018-12-13 11:32:28 +1100

    Add Distinct to FluentSQLQuery

--- Sources/FluentSQL/FluentSQLQuery.swift
+++ Sources/FluentSQL/FluentSQLQuery.swift
@@ -1,13 +1,15 @@
 public protocol FluentSQLQuery {
     associatedtype Statement: FluentSQLQueryStatement
+    associatedtype Distinct: SQLDistinct
     associatedtype Expression: SQLExpression
     associatedtype Join: SQLJoin
     associatedtype OrderBy: SQLOrderBy
     associatedtype GroupBy: SQLGroupBy
     associatedtype TableIdentifier: SQLTableIdentifier
     associatedtype SelectExpression: SQLSelectExpression
-    
+
     var statement: Statement { get set }
+    var distinct: Distinct? { get set }
     var table: TableIdentifier { get set }
     var keys: [SelectExpression] { get set }
     var predicate: Expression? { get set }
@@ -18,7 +20,7 @@ public protocol FluentSQLQuery {
     var offset: Int? { get set }
     var values: [String: Expression] { get set }
     var defaultBinaryOperator: Expression.BinaryOperator { get set }
-    
+
     static func query(_ statement: Statement, _ table: TableIdentifier) -> Self
 }
 
