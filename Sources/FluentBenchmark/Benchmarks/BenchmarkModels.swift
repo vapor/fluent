5708ce3c7f09de84714fa09f0800b62b7f460600
Valerio Mazzeo <valerio.mazzeo@gmail.com>
2018-12-18 11:44:54 +0000

    Fixed improper use of update

--- Sources/FluentBenchmark/Benchmarks/BenchmarkModels.swift
+++ Sources/FluentBenchmark/Benchmarks/BenchmarkModels.swift
@@ -16,7 +16,7 @@ extension Benchmarker where Database: QuerySupporting {
         // update
         b.bar = "fdsa"
         _ = try test(b.save(on: conn))
-        _ = try test(Foo.query(on: conn).filter(\Foo<Database>.id == a.id).update(data: ["baz": 314]))
+        _ = try test(Foo.query(on: conn).filter(\Foo<Database>.id == a.id).update(\.baz, to: 314).run())
 
         // read
         let fetched = try test(Foo<Database>.find(b.requireID(), on: conn))
