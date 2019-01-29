dceddd344ddb6f6f8533a27a90789e92d878d676
twof <fabiobean2@gmail.com>
2019-01-28 17:01:44 -0800

    mass crud methods

--- Sources/Fluent/Model/Collection+Model.swift
+++ Sources/Fluent/Model/Collection+Model.swift
@@ -1,11 +1,53 @@
 import Async
 
-extension Collection where Element: Model, Element.Database: QuerySupporting {
+extension Collection where Element: Model, Element.Database: TransactionSupporting {
     /// Saves a collection of models to Database
     ///
     /// - Parameter conn: A means to connect to a database
     /// - Returns: A future array of the saved models
-    public func save(on conn: DatabaseConnectable) -> Future<[Element]> {
-        return self.map { $0.save(on: conn) }.flatten(on: conn)
+    func save(on conn: Element.Database.Connection) -> Future<[Element]> {
+        let databaseIdentifier = Element.defaultDatabase!
+        
+        return conn.transaction(on: databaseIdentifier) { (dbConn) -> EventLoopFuture<[Element]> in
+            return self.map { $0.save(on: dbConn) }.flatten(on: dbConn)
+        }
+    }
+    
+    /// Updates a collection of models in Database
+    ///
+    /// - Parameter conn: A means to connect to a database
+    /// - Returns: A future array of the updated models
+    public func update(on conn: DatabaseConnectable) -> Future<[Element]> {
+        let databaseIdentifier = Element.defaultDatabase!
+        
+        return conn.transaction(on: databaseIdentifier) { (dbConn) -> EventLoopFuture<[Element]> in
+            return self.map { $0.update(on: conn) }.flatten(on: conn)
+        }
+    }
+    
+    /// Creates a collection of models in Database
+    ///
+    /// - Parameter conn: A means to connect to a database
+    /// - Returns: A future array of the created models
+    func create(on conn: Element.Database.Connection) -> Future<[Element]> {
+        let databaseIdentifier = Element.defaultDatabase!
+        
+        return conn.transaction(on: databaseIdentifier) { (dbConn) -> EventLoopFuture<[Element]> in
+            return self.map { $0.create(on: dbConn) }.flatten(on: dbConn)
+        }
+    }
+    
+    /// Deletes a collection of models from Database
+    ///
+    /// - Parameter conn: A means to connect to a database
+    /// - Returns: A future array of the deleted models
+    func delete(on conn: Element.Database.Connection) -> Future<[Element]> {
+        let databaseIdentifier = Element.defaultDatabase!
+        
+        return conn.transaction(on: databaseIdentifier) { (dbConn) -> EventLoopFuture<[Element]> in
+            return self.map { element in
+                return element.delete(on: dbConn).transform(to: element)
+            }.flatten(on: dbConn)
+        }
     }
 }
