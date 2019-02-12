80649ab831bd78b4f256b62c9261761a07978b7d
Tanner Nelson <me@tanner.xyz>
2019-02-11 19:55:05 -0500

    add teststub + fix circle yml

--- Tests/FluentTests/FluentTests.swift
+++ Tests/FluentTests/FluentTests.swift
@@ -1,1 +1,12 @@
 import Fluent
+import XCTest
+
+final class FluentTests: XCTestCase {
+    func testStub() throws {
+        
+    }
+    
+    static let allTests = [
+        ("testStub", testStub)
+    ]
+}
