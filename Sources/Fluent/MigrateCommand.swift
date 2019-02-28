ebc584baec4b2859fd7b6f0bbabfdf41f931475a
Tanner Nelson <me@tanner.xyz>
2019-02-28 17:17:29 -0500

    add revert command

--- Sources/Fluent/MigrateCommand.swift
+++ Sources/Fluent/MigrateCommand.swift
@@ -6,7 +6,7 @@ public final class MigrateCommand: Command {
     }
     
     public var options: [CommandOption] {
-        return []
+        return [.flag(name: "revert")]
     }
     
     public var help: [String] {
@@ -20,19 +20,60 @@ public final class MigrateCommand: Command {
     }
     
     public func run(using context: CommandContext) throws -> EventLoopFuture<Void> {
+        let isRevert = context.options["revert"].flatMap(Bool.init) ?? false
+        context.console.info("Migrate Command: \(isRevert ? "Revert" : "Prepare")")
+        return self.migrator.setupIfNeeded().flatMap {
+            if isRevert {
+                return self.revert(using: context)
+            } else {
+                return self.prepare(using: context)
+            }
+        }
+    }
+    
+    private func revert(using context: CommandContext) -> EventLoopFuture<Void> {
+        return self.migrator.previewRevertLastBatch().flatMap { migrations in
+            guard migrations.count > 0 else {
+                context.console.print("No migrations to revert.")
+                return context.eventLoop.makeSucceededFuture(())
+            }
+            context.console.print("The following migration(s) will be reverted:")
+            for (migration, dbid) in migrations {
+                context.console.print("- ", newLine: false)
+                context.console.error(migration.name, newLine: false)
+                context.console.print(" on ", newLine: false)
+                context.console.print(dbid?.string ?? "default")
+            }
+            if context.console.confirm("Would you like to continue?".consoleText(.warning)) {
+                return self.migrator.revertLastBatch().map {
+                    context.console.print("Migration successful")
+                }
+            } else {
+                context.console.warning("Migration cancelled")
+                return context.eventLoop.makeSucceededFuture(())
+            }
+        }
+    }
+    
+    private func prepare(using context: CommandContext) -> EventLoopFuture<Void> {
         return self.migrator.previewPrepareBatch().flatMap { migrations in
             guard migrations.count > 0 else {
                 context.console.print("No new migrations.")
                 return context.eventLoop.makeSucceededFuture(())
             }
-            context.console.print("The following migration(s) will prepare:")
+            context.console.print("The following migration(s) will be prepared:")
             for (migration, dbid) in migrations {
-                let id = dbid?.string ?? "default"
-                context.console.print("- \(migration.name) on \(id)")
+                context.console.print("+ ", newLine: false)
+                context.console.success(migration.name, newLine: false)
+                context.console.print(" on ", newLine: false)
+                context.console.print(dbid?.string ?? "default")
             }
-            if context.console.confirm("Would you like to continue?") {
-                return self.migrator.prepareBatch()
+            if context.console.confirm("Would you like to continue?".consoleText(.warning)) {
+                return self.migrator.prepareBatch().map {
+                    context.console.print("Migration successful")
+                }
             } else {
+                context.console.warning("Migration cancelled")
                 return context.eventLoop.makeSucceededFuture(())
             }
         }
