fe952d49033c4be9902dc38bf710a84c20c66b21
Jerry Carter <jerry@jerrycarter.org>
2019-05-02 18:54:13 -0400

    Update BenchmarkSoftDeletable.swift
    Added unit test for deleting a previously soft deleted row.

--- Sources/FluentBenchmark/Benchmarks/BenchmarkSoftDeletable.swift
+++ Sources/FluentBenchmark/Benchmarks/BenchmarkSoftDeletable.swift
@@ -4,38 +4,60 @@ extension Benchmarker where Database: QuerySupporting & TransactionSupporting {
     /// The actual benchmark.
     fileprivate func _benchmark(on conn: Database.Connection) throws {
         start("Soft delete")
-        // create
-        var bar = Bar<Database>(baz: 1)
+        var bar1 = Bar<Database>(baz: 1)
+        var bar2 = Bar<Database>(baz: 2)
 
+        // Initially empty
         if try test(Bar<Database>.query(on: conn).count()) != 0 {
             fail("count should have been 0")
         }
 
-        bar = try test(bar.save(on: conn))
+        // Create two rows
+        bar1 = try test(bar1.save(on: conn))
+        bar2 = try test(bar2.save(on: conn))
+        if try test(Bar<Database>.query(on: conn).count()) != 2 {
+            fail("count should have been 2")
+        }
+        if try test(Bar<Database>.query(on: conn, withSoftDeleted: true).count()) != 2 {
+            fail("count should have been 2")
+        }
+
+        // Soft delete the first
+        try test(bar1.delete(on: conn))
         if try test(Bar<Database>.query(on: conn).count()) != 1 {
             fail("count should have been 1")
         }
-        if try test(Bar<Database>.query(on: conn, withSoftDeleted: true).count()) != 1 {
-            fail("count should have been 1")
+        if try test(Bar<Database>.query(on: conn, withSoftDeleted: true).count()) != 2 {
+            fail("count should have been 2")
         }
 
-        try test(bar.delete(on: conn))
+        // Soft delete the second
+        try test(bar2.delete(on: conn))
+        if try test(Bar<Database>.query(on: conn).count()) != 0 {
+            fail("count should have been 0")
+        }
+        if try test(Bar<Database>.query(on: conn, withSoftDeleted: true).count()) != 2 {
+            fail("count should have been 2")
+        }
+        
+        // Restore the first
+        bar1 = try test(bar1.restore(on: conn))
+        if try test(Bar<Database>.query(on: conn).count()) != 1 {
+            fail("count should have been 1")
+        }
+        if try test(Bar<Database>.query(on: conn, withSoftDeleted: true).count()) != 2 {
+            fail("count should have been 2")
+        }
+
+        // Delete both
+        try test(bar1.delete(force: true, on: conn))
         if try test(Bar<Database>.query(on: conn).count()) != 0 {
             fail("count should have been 0")
         }
         if try test(Bar<Database>.query(on: conn, withSoftDeleted: true).count()) != 1 {
             fail("count should have been 1")
         }
-
-        bar = try test(bar.restore(on: conn))
-        if try test(Bar<Database>.query(on: conn).count()) != 1 {
-            fail("count should have been 1")
-        }
-        if try test(Bar<Database>.query(on: conn, withSoftDeleted: true).count()) != 1 {
-            fail("count should have been 1")
-        }
-
-        try test(bar.delete(force: true, on: conn))
+        try test(bar2.delete(force: true, on: conn))
         if try test(Bar<Database>.query(on: conn).count()) != 0 {
             fail("count should have been 0")
         }
