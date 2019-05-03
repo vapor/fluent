514cf3d991ac7ab485465f0504d6038a4dfea12d
Jerry Carter <jerry@jerrycarter.org>
2019-05-03 10:18:31 -0400

    Update BenchmarkSoftDeletable.swift
    Added a test of the behavior of the updatedAt and deletedAt timestamps under soft delete.

--- Sources/FluentBenchmark/Benchmarks/BenchmarkSoftDeletable.swift
+++ Sources/FluentBenchmark/Benchmarks/BenchmarkSoftDeletable.swift
@@ -66,10 +66,80 @@ extension Benchmarker where Database: QuerySupporting & TransactionSupporting {
         }
     }
 
+    /// Verify that updatedAt and deletedAt are changed appropriately for soft delete.
+    fileprivate func _benchmarkTimestamps(on conn: Database.Connection) throws {
+        start("Soft delete timestamps")
+
+        // Initially empty
+        if try test(Bar<Database>.query(on: conn).count()) != 0 {
+            fail("count should have been 0")
+        }
+
+        // Upon creation, updatedAt should be set and deletedAt should be nil
+        var bar = Bar<Database>(baz: 16)
+        bar = try test(bar.save(on: conn))
+        if bar.deletedAt != nil {
+            fail("deletedAt should not be set")
+        }
+        guard let updatedAtAfterCreate = bar.updatedAt else {
+            fail("updatedAt was not set")
+            return
+        }
+        
+        // Ensure that there is a substantial difference for checking update timestamp.
+        Thread.sleep(forTimeInterval: 0.05)
+
+        // After soft delete, updatedAt should have changed and deletedAt should now be set
+        try test(bar.delete(on: conn))
+        guard let deletedAt = bar.deletedAt else {
+            fail("deletedAt was not set")
+            return
+        }
+        if !deletedAt.isWithin(seconds: 0.1, of: Date()) {
+            fail("timestamps should be current")
+        }
+        if deletedAt <= updatedAtAfterCreate {
+            fail("deletedAt value is invalid")
+        }
+        guard let updatedAtAfterDelete = bar.updatedAt else {
+            fail("updatedAt was cleared after soft delete")
+            return
+        }
+        if updatedAtAfterDelete <= updatedAtAfterCreate {
+            fail("updatedAt was not changed after soft delete")
+        }
+
+        // Ensure that there is a substantial difference for checking update timestamp.
+        Thread.sleep(forTimeInterval: 0.05)
+
+        // After reset, updatedAt should have changed and deletedAt should now be nil
+        bar = try test(bar.restore(on: conn))
+        if bar.deletedAt != nil {
+            fail("deletedAt was not cleared after restore")
+        }
+        if bar.updatedAt == nil {
+            fail("updateAt was cleared after restore")
+        }
+        guard let updatedAtAfterRestore = bar.updatedAt else {
+            fail("updatedAt was cleared after restore")
+            return
+        }
+        if updatedAtAfterRestore <= updatedAtAfterDelete {
+            fail("updatedAt was not changed after restore")
+        }
+
+        // Clean up
+        try test(Bar<Database>.query(on: conn, withSoftDeleted: true).delete(force: true))
+        if try test(Bar<Database>.query(on: conn).count()) != 0 {
+            fail("count should be 0 at the end of the test")
+        }
+    }
+    
     /// Benchmark fluent transactions.
     public func benchmarkSoftDeletable() throws {
         let conn = try test(pool.requestConnection())
         try self._benchmark(on: conn)
+        try self._benchmarkTimestamps(on: conn)
         pool.releaseConnection(conn)
     }
 }
@@ -87,5 +157,3 @@ extension Benchmarker where Database: QuerySupporting & TransactionSupporting & 
         pool.releaseConnection(conn)
     }
 }
-
-
