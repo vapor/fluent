bd2d0b5378b45f329b90d4cf310cd078f3dc6638
Gwynne Raskind <gwynne@darkrainfall.org>
2020-02-26 16:59:49 -0600

    Require the beta versions we actually require of Vapor and FluentKit

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
         .library(name: "Fluent", targets: ["Fluent"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/fluent-kit.git", from: "1.0.0-beta.2"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta.2"),
+        .package(url: "https://github.com/vapor/fluent-kit.git", from: "1.0.0-beta.5"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta.4"),
     ],
     targets: [
         .target(name: "Fluent", dependencies: ["FluentKit", "Vapor"]),
