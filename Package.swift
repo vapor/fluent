288e998497c2a1cdbc043cf624b4a8f292c9af23
tanner0101 <me@tanner.xyz>
2020-04-09 13:33:48 -0400

    use fk branch

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(name: "Fluent", targets: ["Fluent"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/fluent-kit.git", from: "1.0.0-rc.1"),
+        .package(url: "https://github.com/vapor/fluent-kit.git", .branch("vapor-gm")),
         .package(url: "https://github.com/vapor/vapor.git", .branch("gm")),
     ],
     targets: [
