7bbc61c87f7e6c2aa72cfe7d52f388f06a011444
tanner0101 <me@tanner.xyz>
2020-05-29 17:34:51 -0400

    version update

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(name: "Fluent", targets: ["Fluent"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/fluent-kit.git", .branch("gm")),
+        .package(url: "https://github.com/vapor/fluent-kit.git", from: "1.0.0-rc.2"),
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
     ],
     targets: [
