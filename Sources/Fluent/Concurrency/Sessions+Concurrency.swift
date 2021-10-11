cb3a432315a3c0044e9f9c71d8610033b5723cc3
Tim <0xtimc@gmail.com>
2021-10-11 12:45:21 +0100

    Add async sessions

--- Sources/Fluent/Concurrency/Sessions+Concurrency.swift
+++ Sources/Fluent/Concurrency/Sessions+Concurrency.swift
@@ -11,50 +11,6 @@ extension Model where Self: SessionAuthenticatable, Self.SessionID == Self.IDVal
     }
 }
 
-private struct DatabaseSessions: SessionDriver {
-    let databaseID: DatabaseID?
-    
-    init(databaseID: DatabaseID? = nil) {
-        self.databaseID = databaseID
-    }
-    
-    func createSession(_ data: SessionData, for request: Request) -> EventLoopFuture<SessionID> {
-        let id = self.generateID()
-        return SessionRecord(key: id, data: data)
-            .create(on: request.db(self.databaseID))
-            .map { id }
-    }
-    
-    func readSession(_ sessionID: SessionID, for request: Request) -> EventLoopFuture<SessionData?> {
-        SessionRecord.query(on: request.db(self.databaseID))
-            .filter(\.$key == sessionID)
-            .first()
-            .map { $0?.data }
-    }
-    
-    func updateSession(_ sessionID: SessionID, to data: SessionData, for request: Request) -> EventLoopFuture<SessionID> {
-        SessionRecord.query(on: request.db(self.databaseID))
-            .filter(\.$key == sessionID)
-            .set(\.$data, to: data)
-            .update()
-            .map { sessionID }
-    }
-    
-    func deleteSession(_ sessionID: SessionID, for request: Request) -> EventLoopFuture<Void> {
-        SessionRecord.query(on: request.db(self.databaseID))
-            .filter(\.$key == sessionID)
-            .delete()
-    }
-    
-    private func generateID() -> SessionID {
-        var bytes = Data()
-        for _ in 0..<32 {
-            bytes.append(.random(in: .min ..< .max))
-        }
-        return .init(string: bytes.base64EncodedString())
-    }
-}
-
 @available(macOS 12, iOS 15, watchOS 8, tvOS 15, *)
 private struct AsyncDatabaseSessionAuthenticator<User>: AsyncSessionAuthenticator
     where User: SessionAuthenticatable, User: Model, User.SessionID == User.IDValue
