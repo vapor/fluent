d8135fbd17cee3c3d0c4202d2d0be7ed551819b4
Tim <0xtimc@gmail.com>
2021-10-11 13:49:43 +0100

    Add async versions of migrate and revert

---
+++ Sources/Fluent/Concurrency/FluentProvider+Concurrency.swift
@@ -0,0 +1,24 @@
+#if compiler(>=5.5) && canImport(_Concurrency)
+import NIOCore
+import Vapor
+
+@available(macOS 12, iOS 15, watchOS 8, tvOS 15, *)
+extension Application {
+    /// Automatically runs forward migrations without confirmation.
+    /// This can be triggered by passing `--auto-migrate` flag.
+    public func autoMigrate() async throws {
+        try await self.migrator.setupIfNeeded().flatMap {
+            self.migrator.prepareBatch()
+        }.get()
+    }
+
+    /// Automatically runs reverse migrations without confirmation.
+    /// This can be triggered by passing `--auto-revert` during boot.
+    public func autoRevert() async throws {
+        try await self.migrator.setupIfNeeded().flatMap {
+            self.migrator.revertAllBatches()
+        }.get()
+    }
+}
+
+#endif
